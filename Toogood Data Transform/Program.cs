2e85740ea4299bc697993d5780f724f26c094498
Thomas Zeitoun <thomaszeitoun@gmail.com>
2019-06-24 17:55:35 -0400

    Add project files.

---
+++ Toogood Data Transform/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Toogood_Data_Transform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
