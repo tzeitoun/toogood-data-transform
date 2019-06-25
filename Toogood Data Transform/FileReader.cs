9b048600d4bb43ff242d03dbaba103bc3760157e
Thomas Zeitoun <thomaszeitoun@gmail.com>
2019-06-24 21:36:46 -0400

    initial

---
+++ Toogood Data Transform/FileReader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Toogood_Data_Transform
+{
+    /// <summary>
+    /// This class will serve as the file input. It will read the files
+    /// from CSV (not implemented), and store the data as an array of TargetRecord objects.
+    /// </summary>
+    /// 
+
+    enum InputFileType
+    {
+        Type1,
+        Type2
+    }
+
+    class FileReader
+    {
+        public List<TargetRecord> records { private set; get; }
+
+        /// <summary>
+        /// Create a new FileReader for the specified input file type.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="inputFileType"></param>
+        public FileReader(string filename, InputFileType inputFileType)
+        {
+            records = new List<TargetRecord>();
+
+            // Open CSV file...
+
+            int accountTypeLength = Enum.GetNames(typeof(AccountType)).Length;
+            int currencyTypeLength = Enum.GetNames(typeof(CurrencyType)).Length;
+
+            DateTime startDate = DateTime.Now;
+
+            // Read and parse CSV data here...  (Actually generate pretend records)
+            for (int i=0; i < 10; i++)
+            {
+                TargetRecord targetRecord = new TargetRecord(
+                    "ACNTCODE" + i,
+                    "ACNTNAME" + i,
+                    (AccountType)(i % accountTypeLength),
+                    startDate.AddDays(i),
+                    (CurrencyType)(i % currencyTypeLength)
+                    );
+                records.Add(targetRecord);
+            }
+            
+        }
+    }
+}
---
+++ Toogood Data Transform/TargetRecord.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Toogood_Data_Transform
+{
+    enum AccountType
+    {
+        Trading = 1,
+        RRSP = 2,
+        RESP = 3,
+        Fund = 4
+    }
+
+    enum CurrencyType
+    {
+        CAD = 0,
+        USD = 1
+    }
+
+    class TargetRecord
+    {
+        string AccountCode;
+        string Name;
+        AccountType Type;
+        DateTime OpenDate;
+        CurrencyType Currency;
+
+        /// <summary>
+        /// Creates a new record with the specified parameters.
+        /// </summary>
+        /// <param name="accountCode"></param>
+        /// <param name="name"></param>
+        /// <param name="type"></param>
+        /// <param name="openDate"></param>
+        /// <param name="currency"></param>
+        public TargetRecord(
+            string accountCode,
+            string name,
+            AccountType type,
+            DateTime openDate,
+            CurrencyType currency
+            )
+        {
+            this.AccountCode = accountCode;
+            this.Name = name;
+            this.Type = type;
+            this.OpenDate = openDate;
+            this.Currency = currency;
+        }
+
+        /// <summary>
+        /// Transforms the record into the standard format and returns it as a string.
+        /// </summary>
+        /// <returns>The record in the standard format as a string.</returns>
+        public string getRecord()
+        {
+            string record = AccountCode
+                + "\t" + Name
+                + "\t" + Type
+                + "\t" + OpenDate
+                + "\t" + Currency
+                ;
+            return record;
+        }
+    }
+
+}
